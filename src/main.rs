90747d0202c9952e09b5a468805cf1036a3e3891
Arthur <arthrp@users.noreply.github.com>
2021-02-12 01:48:29 +0200

    Adding ability to use other model folder

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use std::env;
 fn main() {
     let args : Vec<String> = env::args().collect();
     if (args.len() < 2){
-        println!("Usage: rust-ocr-test [path]");
+        println!("Usage: rust-ocr-test [path to file] -f [path to models folder]");
         return;
     }
 
-    let mut lt = leptess::LepTess::new(Some("./data"), "eng").unwrap();
+    let lang_folder = if args.len() > 2 && args[2] == "-f" { &args[3] } else { "./data" };
+    let mut lt = leptess::LepTess::new(Some(lang_folder), "eng").unwrap();
     lt.set_image(&args[1]);
     println!("{}", lt.get_utf8_text().unwrap());
 }
