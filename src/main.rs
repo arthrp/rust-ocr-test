51b46821aa5d537bb86376870d26f769bec925c5
Arthur <arthrp@users.noreply.github.com>
2021-02-14 01:01:12 +0200

    Adding ability to use non-english models (english is still default)

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::env;
 fn main() {
     let args : Vec<String> = env::args().collect();
     if (args.len() < 2){
-        println!("Usage: rust-ocr-test [path to file] -f [path to models folder]");
+        println!("Usage: rust-ocr-test [path to file] -f [path to models folder (optional)] -l [language (optional)]");
         return;
     }
 
     let lang_folder = if args.len() > 2 && args[2] == "-f" { &args[3] } else { "./data" };
-    let mut lt = leptess::LepTess::new(Some(lang_folder), "eng").unwrap();
+    let lang = if args.len() > 5 && args[4] == "-l" { &args[5] } else { "eng" };
+    let mut lt = leptess::LepTess::new(Some(lang_folder), lang).unwrap();
     lt.set_image(&args[1]);
     println!("{}", lt.get_utf8_text().unwrap());
 }
